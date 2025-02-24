307fbf5541fcece937898be1f95ad06525e4b471
Savarelav18 <savarelav@unal.edu.co>
2025-02-23 22:32:33 -0500

    Laboratorio terminado

--- homework/homework.py
+++ homework/homework.py
@@ -102,7 +102,14 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.svm import SVC
-from sklearn.metrics import balanced_accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, make_scorer
+from sklearn.metrics import (
+    balanced_accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix,
+    make_scorer,
+)
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_selection import SelectKBest
 from sklearn.pipeline import Pipeline
@@ -117,6 +124,7 @@ import pickle
 import zipfile
 import pandas as pd
 
+
 def load_data():
 
     dataframe_test = pd.read_csv(
@@ -127,15 +135,16 @@ def load_data():
 
     dataframe_train = pd.read_csv(
         "./files/input/train_data.csv.zip",
-        index_col = False,
-        compression ="zip",
+        index_col=False,
+        compression="zip",
     )
 
     return dataframe_train, dataframe_test
 
+
 def clean_data(df):
     df_copy = df.copy()
-    df_copy = df_copy.rename(columns={'default payment next month' : "default"})
+    df_copy = df_copy.rename(columns={"default payment next month": "default"})
     df_copy = df_copy.drop(columns=["ID"])
     df_copy = df_copy.loc[df["MARRIAGE"] != 0]
     df_copy = df_copy.loc[df["EDUCATION"] != 0]
@@ -143,36 +152,45 @@ def clean_data(df):
     df_copy = df_copy.dropna()
     return df_copy
 
+
 def split_data(df):
     return df.drop(columns=["default"]), df["default"]
-    
+
+
 def make_pipeline(x_train):
-    categorical_columns = ["SEX","EDUCATION","MARRIAGE"]
+    categorical_columns = ["SEX", "EDUCATION", "MARRIAGE"]
     numerical_columns = list(set(x_train.columns).difference(categorical_columns))
     preprocessor = ColumnTransformer(
         transformers=[
-            ('onehot', OneHotEncoder(handle_unknown='ignore'), categorical_columns), 
-            ("scaler", StandardScaler(with_mean=True, with_std=True), numerical_columns),
+            ("onehot", OneHotEncoder(handle_unknown="ignore"), categorical_columns),
+            (
+                "scaler",
+                StandardScaler(with_mean=True, with_std=True),
+                numerical_columns,
+            ),
         ],
-        remainder='passthrough'  
+        remainder="passthrough",
     )
-    pipeline = Pipeline([
-        ('preprocessor', preprocessor),       
-        ('pca', PCA()),              
-        ('feature_selection', SelectKBest(score_func=f_classif)),       
-        ('classifier', SVC(kernel="rbf", random_state=12345, max_iter=-1))                       
-    ])
+    pipeline = Pipeline(
+        [
+            ("preprocessor", preprocessor),
+            ("pca", PCA()),
+            ("feature_selection", SelectKBest(score_func=f_classif)),
+            ("classifier", SVC(kernel="rbf", random_state=12345, max_iter=-1)),
+        ]
+    )
 
     return pipeline
 
+
 def create_estimator(pipeline, x_train):
-    
+
     param_grid = {
-    "pca__n_components": [20, x_train.shape[1] - 2],  
-    'feature_selection__k': [12],           
-    'classifier__kernel': ["rbf"],           
-    'classifier__gamma': [0.1],                  
-    }   
+        "pca__n_components": [20, x_train.shape[1] - 2],
+        "feature_selection__k": [12],
+        "classifier__kernel": ["rbf"],
+        "classifier__gamma": [0.1],
+    }
     cv = StratifiedKFold(n_splits=10)
     scorer = make_scorer(balanced_accuracy_score)
     grid_search = GridSearchCV(
@@ -181,24 +199,26 @@ def create_estimator(pipeline, x_train):
         scoring=scorer,
         cv=cv,
         n_jobs=-1,
-        verbose=3 
+        verbose=3,
     )
 
     return grid_search
-    
+
+
 def _create_output_directory(output_directory):
     if os.path.exists(output_directory):
-        for file in glob(f"{output_directory}/*"):
+        for file in glob.glob(f"{output_directory}/*"):
             os.remove(file)
         os.rmdir(output_directory)
     os.makedirs(output_directory)
-    
+
+
 def _save_model(path, estimator):
     _create_output_directory("files/models/")
 
     with gzip.open(path, "wb") as f:
         pickle.dump(estimator, f)
-        
+
 
 def calculate_metrics(dataset_type, y_true, y_pred):
     """Calculate metrics"""
@@ -210,7 +230,8 @@ def calculate_metrics(dataset_type, y_true, y_pred):
         "recall": recall_score(y_true, y_pred, zero_division=0),
         "f1_score": f1_score(y_true, y_pred, zero_division=0),
     }
-    
+
+
 def calculate_confusion(dataset_type, y_true, y_pred):
     """Confusion matrix"""
     cm = confusion_matrix(y_true, y_pred)
@@ -220,6 +241,7 @@ def calculate_confusion(dataset_type, y_true, y_pred):
         "true_0": {"predicted_0": int(cm[0][0]), "predicted_1": int(cm[0][1])},
         "true_1": {"predicted_0": int(cm[1][0]), "predicted_1": int(cm[1][1])},
     }
+
 
 def _run_jobs():
     data_train, data_test = load_data()
